38b3e2aa524cb4f7aa941d849cffd0e675a2db56
St0g0vAndrey <stogovand@icloud.com>
2021-11-15 02:08:05 +1000

    6

--- 6|_StogovAndrey.playground/Contents.swift
+++ 6|_StogovAndrey.playground/Contents.swift
@@ -1,3 +1,41 @@
-import Cocoa
+import PlaygroundSupport
+import Foundation
 
-var greeting = "Hello, playground"
+struct SomeStruct <MyNewTYpe> {
+    var queue = [MyNewTYpe] ()
+    
+    
+    mutating func push (_ elment: MyNewTYpe) {
+        queue.append(elment)
+    }
+    
+    func FilterNumbers() -> [MyNewTYpe] {
+        let newArray = queue.filter { $0 as! Int % 2 == 0 }
+        return newArray
+    }
+    
+    func FilterString() -> [MyNewTYpe] {
+        let newArray = queue.sorted {$0<$1}
+        return newArray
+        
+    }
+    func PrintArray () {
+        for i in queue {
+            print(i)
+        }
+    }
+}
+
+var Animal = SomeStruct<String>()
+Animal.push("Кот")
+Animal.push("Собака")
+Animal.push("Хомяк")
+
+var Numbers = SomeStruct<Int>()
+Numbers.push(12)
+Numbers.push(9)
+Numbers.push(200)
+
+
+Numbers.PrintArray()
+Numbers.FilterNumbers()
